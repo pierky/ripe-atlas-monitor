82e25ee81fcf2d2cbf0f345295ce8879c11b3996
Pier Carlo Chiodi <pierky@pierky.com>
2016-03-24 21:38:03 +0100

    ipdetailscache 0.4.7

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         "python-dateutil>=1.0, != 2.0",
         "ripe.atlas.cousteau>=1.0.7",
         "ripe.atlas.sagan>=1.1.8",
-        "ipdetailscache>=0.4.6",
+        "ipdetailscache>=0.4.7",
         "pyyaml",
         "pytz",
         "six>=1.10.0"
